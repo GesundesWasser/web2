20a68af9920a47908813d370497ccf0aca0c8b79
GesundesWasser <83233602+GesundesWasser@users.noreply.github.com>
2024-02-24 16:46:04 +0100

    a

--- dev/config.php
+++ dev/config.php
@@ -6,10 +6,10 @@ define('DB_PASSWORD', '#K31N3-B0D3NK4PS3L!');
 define('DB_NAME', 'wwagodata');
 
 /* Attempt to connect to MySQL database */
-$mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
 // Check connection
-if($mysqli === false){
-    die("ERROR: Could not connect. " . $mysqli->connect_error);
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 ?>
