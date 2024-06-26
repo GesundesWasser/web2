3f089f74d0d77b881a2f04b36b615fb0469da3e8
GesundesWasser <gesundeswasser@duck.com>
2024-06-26 21:44:29 +0200

    Update 1.8

--- main.js
+++ main.js
@@ -6,5 +6,6 @@
 window.onload = function() {
   var currentYear = new Date().getFullYear();
   document.getElementById('copyright-year').innerText = currentYear;
-  document.getElementById('site-name-version').innerText = "Codename Kapselordnung v1.8 (Testbuild)";
+  var codename = "Codename Kapselordnung v1.8";
+  document.getElementById('site-name-version').innerText = codename;
 };
