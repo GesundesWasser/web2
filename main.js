baf445866eb7e4e6a62783dc79bd368e8b895701
GesundesWasser <gesundeswasser@duck.com>
2024-06-30 19:27:38 +0200

    We're Redesigning the ENTIRE Site.

--- main.js
+++ main.js
@@ -1,10 +1,70 @@
-// ---------------------------
-// Main JS File of the Site
-// Copyright mcdonelts.city
-// ---------------------------
-
 window.onload = function() {
   var currentYear = new Date().getFullYear();
   document.getElementById('copyright-year').innerText = currentYear;
-  document.getElementById('site-name-version').innerText = "Codename Kapselordnung v1.8 (Testbuild)";
+  document.getElementById('site-name-version').innerText = "Codename Kapselordnung v1.9 -> R01";
+  document.getElementById('site-title').innerText = "Kapselordnung";
+};
+
+// DOM Elements
+
+const darkButton = document.getElementById('dark');
+const lightButton = document.getElementById('light');
+const solarButton = document.getElementById('solar');
+const body = document.body;
+
+
+// Apply the cached theme on reload
+
+const theme = localStorage.getItem('theme');
+const isSolar = localStorage.getItem('isSolar');
+
+if (theme) {
+  body.classList.add(theme);
+  isSolar && body.classList.add('solar');
+  if (isSolar) {
+    solarButton.innerText = "normalize";
+    solarButton.style.cssText = `--bg-solar: white;`;
+  }
+} else {
+    body.classList.add('light');
+    localStorage.setItem('theme', 'light');
+}
+
+// Button Event Handlers
+
+darkButton.onclick = () => {
+  body.classList.replace('light', 'dark');
+  localStorage.setItem('theme', 'dark');
+};
+
+lightButton.onclick = () => {
+  body.classList.replace('dark', 'light');
+
+  localStorage.setItem('theme', 'light');
+};
+
+solarButton.onclick = () => {
+
+  if (body.classList.contains('solar')) {
+    
+    body.classList.remove('solar');
+    solarButton.style.cssText = `
+      --bg-solar: var(--yellow);
+    `
+
+    solarButton.innerText = 'solarize';
+
+    localStorage.removeItem('isSolar');
+
+  } else {
+
+    solarButton.style.cssText = `
+      --bg-solar: white;
+    `
+
+    body.classList.add('solar');
+    solarButton.innerText = 'normalize';
+
+    localStorage.setItem('isSolar', true);
+  }
 };
